d48f696145218d2113d32738f97aee4dff75df07
nimesh-korat <nimeshkorat611@gmail.com>
2023-08-06 10:49:57 +0530

    resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,5 +4,5 @@ export const headerData = {
     title: "Flutter Developer",
     desciption: "Remember, success is not defined by how many times you fall, but by how many times you rise. Be persistent, be resilient, and never lose sight of your goals. The path ahead may seem daunting at times, but with determination and a positive mindset, you can conquer anything.",
     image: 'https://avatars.githubusercontent.com/u/77136539?v=4    ',
-    resumePdf: "https://drive.google.com/file/d/1jV4103JGtCJKAnIwGweakRAah6wIW7Yo/view?usp=sharing"
+    resumePdf: "https://drive.google.com/file/d/1b2jsYyJyrUMhiP-oXX4DTwQ4XMYmIdJd/view?usp=sharing"
 }
